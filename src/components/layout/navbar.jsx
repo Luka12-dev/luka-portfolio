b5b891b3c248f6733b2fd1a9c822bf938f733eee
Luka <lukatijanic12@gmail.com>
2026-01-21 19:42:44 +0100

    Add src/components/layout/navbar.jsx

---
+++ src/components/layout/navbar.jsx
@@ -0,0 +1,98 @@
+import React, { useRef, useEffect } from "react";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+import projectsIcon from "../../assets/navbar/projects.svg";
+import homeIcon from "../../assets/navbar/home.svg";
+import aboutIcon from "../../assets/navbar/about.svg";
+import contactIcon from "../../assets/navbar/contact.svg";
+import githubIcon from "../../assets/navbar/github.svg";
+import youtubeIcon from "../../assets/navbar/youtube.svg";
+import IconWithTooltips from "../ui/icons-with-tooltips";
+gsap.registerPlugin(ScrollTrigger);
+
+const Navbar = () => {
+  const navbarRef = useRef(null);
+  useEffect(() => {
+    const ctx = gsap.context(() => {
+      gsap.fromTo(
+        navbarRef.current,
+        { translateY: "200px", opacity: 0 },
+        {
+          translateY: "0px",
+          opacity: 1,
+          ease: "expo.out",
+          duration: 0.5,
+          scrollTrigger: {
+            trigger: navbarRef.current,
+            start: "-=500 center",
+            end: "+=350",
+            toggleActions: "play none none reverse",
+            scrub: 1,
+          },
+        }
+      );
+    });
+    return () => ctx.revert();
+  }, []);
+
+  return (
+    <div className="flex    justify-center ">
+      <nav
+        ref={navbarRef}
+        className="xs:hidden md:flex md:fixed md:opacity-100 md:bottom-5 xs:opacity-0  xs:bottom-0 flex justify-center w-[30%] "
+      >
+        <div className="flex flex-row p-3 group  rounded-full items-center gap-3 w-max h-max px-4 bg-dark/80  backdrop-blur-sm supports-[backdrop-filter]:bg-background/60 transform-gpu [border:1px_solid_var(--color-dark)] [box-shadow:0_-20px_80px_-20px_var(--color-bg)_inset] scale-85 transition-all duration-300 hover:scale-90 md:z-10 xs:-z-10 ">
+          <IconWithTooltips
+            title="Home"
+            className="navbar-icon"
+            icon={homeIcon}
+            section="hero-section"
+          />
+          <IconWithTooltips
+            title="About"
+            className="navbar-icon"
+            icon={aboutIcon}
+            section="about-section"
+          />
+          <IconWithTooltips
+            title="Projects"
+            className="navbar-icon"
+            icon={projectsIcon}
+            section="project-section"
+          />
+          <IconWithTooltips
+            title="Contact"
+            className="navbar-icon"
+            icon={contactIcon}
+            section="contact-section"
+          />
+
+          <span className="scale-125 group-hover:scale-150 transition duration-300 font-thin self-center pointer-events-none text-gray">
+            |
+          </span>
+          <div className="flex flex-row items-center gap-2 w-max h-max">
+            <a href="https://github.com/Luka12-dev" target="_blank">
+              <img
+                className="navbar-icon !h-5  !object-cover"
+                src={githubIcon}
+                alt="github icon"
+              />
+            </a>
+            <a
+              href="https://www.youtube.com/@LukaCyber-s4b7o"
+              target="_blank"
+            >
+              <img
+                className="navbar-icon !h-6 "
+                src={youtubeIcon}
+                alt="youtube icon"
+              />
+            </a>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
